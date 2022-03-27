92f0fb30ba2c619a442eb9da26c2bd67f007945b
JicksonHD <jacques.ghajar@lau.edu>
2022-03-28 02:28:16 +0300

    images and animation

--- Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
+++ Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.currency_converter_20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView welcome;
+    TextView name;
+    ImageView converter_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        welcome = (TextView) findViewById(R.id.Welcome_msg);
 
+        name = (TextView) findViewById(R.id.name);
+
+        converter_logo = (ImageView) findViewById(R.id.converter);
+        converter_logo.setTranslationX(-1000);
+        converter_logo.animate().translationXBy(1000).setDuration(2000);
     }
 }
