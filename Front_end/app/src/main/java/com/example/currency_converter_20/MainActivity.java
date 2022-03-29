d1285923fcc46bd66ca2b25a69875a6e5b4b350b
JicksonHD <jacques.ghajar@lau.edu>
2022-03-29 18:12:57 +0300

    intent added

--- Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
+++ Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.currency_converter_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView name;
+    EditText name;
     ImageView converter_logo;
 
     @Override
@@ -16,10 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (TextView) findViewById(R.id.name);
+        name = (EditText) findViewById(R.id.name);
 
         converter_logo = (ImageView) findViewById(R.id.converter);
         converter_logo.setTranslationX(-1000);
         converter_logo.animate().translationXBy(1000).setDuration(2000);
+
+    }
+
+    public void start(View view){
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivity(intent);
     }
 }
