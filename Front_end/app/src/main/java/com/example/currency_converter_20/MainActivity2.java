180bc85b407b1524d0fecc28655cf0cc0c345e94
JicksonHD <jacques.ghajar@lau.edu>
2022-03-31 20:31:11 +0300

    page 2 programming version 2.0

--- Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity2.java
+++ Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,15 +15,17 @@ public class MainActivity2 extends AppCompatActivity {
     EditText lebanese;
     EditText usa;
     TextView result;
+    Button convert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        lebanese = (EditText) findViewById(R.id.lbp);
-        usa = (EditText) findViewById(R.id.usd);
+        lebanese = (EditText) findViewById(R.id.lebanese);
+        usa = (EditText) findViewById(R.id.usa);
         result = (TextView) findViewById(R.id.result);
+        convert = (Button) findViewById(R.id.converter);
 
         Intent x = getIntent();
         String name = x.getStringExtra("name");
@@ -36,21 +39,23 @@ public class MainActivity2 extends AppCompatActivity {
         double output_lebanese = 0;
         double output_usa = 0;
 
-        if (lebanese == null){
+        if (lebanese.getText().toString() == null && usa.getText().toString() != null){
 
             double input_usd = Double.parseDouble(usa.getText().toString());
             output_lebanese = input_usd*25000;
             lebanese.setText(output_lebanese + " LBP");
+
         }
 
-        else if (usa == null){
+        else if (usa.getText().toString() == null && lebanese.getText().toString() != null){
 
             double input_lbp = Double.parseDouble(lebanese.getText().toString());
             output_usa = input_lbp/25000;
             usa.setText(output_usa + " $");
+
         }
 
-        else if (lebanese == null && usa == null){
+        else if (lebanese.getText().toString() == null && usa.getText().toString() == null){
 
             result.setText("You must enter either in LBP or in $");
         }
