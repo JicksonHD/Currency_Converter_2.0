26d4b38f9bcc4bebc67408a1646d4e7ce33a53dd
SleimanDGhanem <sleiman.ghanem@outlook.com>
2022-04-02 23:34:14 +0300

    finished backend retrieved buy rate

--- back_end/project.php
+++ back_end/project.php
@@ -1,14 +1,34 @@
 <?php
-    $ch = curl_init();
+    date_default_timezone_set('Asia/Beirut');
 
-    curl_setopt($ch, CURLOPT_URL, 'https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20224216');
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $server_response = curl_exec($ch);
-
-    curl_close($ch);
-
-    $server_response = json_decode($server_response);
-    echo "<pre>";print_r($server_response);echo "</pre>";
+    $year = intval(date('Y'));
+    $month = intval(date('m'));
+    $day = intval(date('d'));
+    $hour = intval(date('H'));
+    
+    
+    // Creating the variant part of the api which is the date and time
+    $variant_part = strval($year.$month.$day.$hour);
+    
+    $url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t$variant_part";
+    
+    $ch_session = curl_init();
+    
+    curl_setopt($ch_session, CURLOPT_URL, $url);
+    curl_setopt($ch_session, CURLOPT_RETURNTRANSFER, true);
+    
+    $response = curl_exec($ch_session);
+    
+    curl_close($ch_session);
+    
+    $server_response = (array) json_decode($response,true);
+    $array_length =  sizeof($server_response['buy']);
+    
+    $json = [];
+    $json ["Rate"] = (array) $server_response['buy'][$array_length - 1][1];
+    
+    
+    $json_response = json_encode($json);
+    echo $json_response;
+    
 ?>
