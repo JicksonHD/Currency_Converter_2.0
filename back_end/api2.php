6d75349e7991978ecbb40a5cf02aaf38c79b03a8
SleimanDGhanem <sleiman.ghanem@outlook.com>
2022-04-04 00:15:22 +0300

    added comments to the 2nd api

--- back_end/api2.php
+++ back_end/api2.php
@@ -5,9 +5,9 @@
   $current_rate = $_POST["current_rate"];
   $currency = $_POST["currency"];
   
-  $query = $mysqli->prepare("INSERT INTO conversions(amount, current_rate, currency) VALUES (?,?,?)");
+  $query = $mysqli->prepare("INSERT INTO conversions(amount, current_rate, currency) VALUES (?,?,?)");//preparing to send to database
 
-  $query->bind_param("iis", $amount, $current_rate, $currency);
+  $query->bind_param("iis", $amount, $current_rate, $currency);//adding the values to be sent to the database
   $query->execute();
 
 ?>
