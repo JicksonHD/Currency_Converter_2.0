cd19ce4488a7458eb40e70e68d74ba6dbc7e3056
JicksonHD <jacques.ghajar@lau.edu>
2022-04-03 23:44:43 +0300

    edited in coding of first image

--- Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
+++ Front_end/app/src/main/java/com/example/currency_converter_20/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     //Declaring variables used in UI
     EditText name;
     ImageView converter_logo;
+    boolean name_is_entered;
 
 
     //API and database connection with front end
@@ -66,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         //Declaring the variables used in UI
         name = (EditText) findViewById(R.id.name);
         converter_logo = (ImageView) findViewById(R.id.converter);
+        name_is_entered = false;
         //Adding animation to logo
         converter_logo.setTranslationX(-1000);
         converter_logo.animate().translationXBy(1000).setDuration(2000);
@@ -75,11 +78,20 @@ public class MainActivity extends AppCompatActivity {
     //Onclick method used on start button
     public void start(View view){
 
-        //Declaring and initializing variables that are being sent to page 2
-        String entered_name = name.getText().toString();
-        Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-        intent.putExtra("name", entered_name);
-        startActivity(intent);
+
+            //Declaring and initializing variables that are being sent to page 2
+            String entered_name = name.getText().toString();
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            intent.putExtra("name", entered_name);
+            startActivity(intent);
+            name_is_entered = true;
+
+            if (name_is_entered){
+
+
+        }else {
+                Toast.makeText(getApplicationContext(),"You must enter your name (legislative reasons)",Toast.LENGTH_LONG).show();
+            }
 
         String url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233120";
 
